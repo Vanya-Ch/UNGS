b04b229c3887bef6031fdf34ff0c075df3e85659
Vanya-Ch <mr.vanya882@gmail.com>
2024-12-04 12:36:50 +0200

    fix rent dates

--- server/controllers/rentCar-controller.js
+++ server/controllers/rentCar-controller.js
@@ -27,6 +27,14 @@ const isCarAvailable = async (car, startTime, endTime) => {
 const addRentCar = async (req, res) => {
     const { car, driver, startPoint, destination, time, comment, passanger, typeOfTravel } = req.body;
     const { startTime, endTime } = time[0];
+
+    const now = new Date(); // Поточна дата і час
+    now.setHours(0, 0, 0, 0); // Обнулюємо час для порівняння лише дати
+
+    // Перевірка, що початковий час не раніше сьогоднішнього дня
+    if (new Date(startTime) < now) {
+        return res.status(400).json({ error: 'Початковий час не може бути раніше сьогоднішнього дня' });
+    }
     
     if (new Date(endTime) <= new Date(startTime)) {
         return res.status(400).json({ error: 'Кінцевий час не може бути раніше або рівний початковому часу' });
